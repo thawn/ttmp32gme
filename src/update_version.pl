c5e5922477e7ad8b313ed10ed572661f3bbc147c
Till Korten <webmaster@korten.at>
2021-02-14 17:43:49 +0100

    updated version string

--- src/update_version.pl
+++ src/update_version.pl
@@ -9,7 +9,7 @@ use Perl::Version;
 use DBI;
 use DBIx::MultiStatementDo;
 
-my $version_str = "0.3.3";
+my $version_str = "0.3.4";
 
 # Command line startup options
 #  Usage: update_version [-v|--version]
