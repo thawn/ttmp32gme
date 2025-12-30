48489ee57ead7f897f5611409d2b2793331d820d
Till Korten <webmaster@korten.at>
2025-12-30 20:05:46 +0100

    changed to non-snap chromium on development machine

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -42,7 +42,7 @@ def chrome_options():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    options.BinaryLocation = "/usr/bin/chromium-browser"
+    options.BinaryLocation = "/usr/bin/chromium"
     return options
 
 
